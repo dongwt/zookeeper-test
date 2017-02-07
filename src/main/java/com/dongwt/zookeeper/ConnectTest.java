5152008925b7761ad32b4d8deec3a6ed5dca91d5
董 纹陶 [SVR III] <dongwentao@lifang.com>
2017-02-07 17:34:48 +0800

    watch

--- src/main/java/com/dongwt/zookeeper/ConnectTest.java
+++ src/main/java/com/dongwt/zookeeper/ConnectTest.java
@@ -4,16 +4,20 @@ import org.apache.curator.RetryPolicy;
 import org.apache.curator.RetrySleeper;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.junit.Before;
+import org.junit.Test;
 
 public class ConnectTest {
-    
-    public static void main(String[] args) throws Exception{
-        
-        String connectString = "192.168.188.129";
-        int sessionTimeoutMs = 5000;
-        int connectionTimeoutMs = 5000;
-        
-        CuratorFramework client = CuratorFrameworkFactory.newClient(connectString, sessionTimeoutMs, connectionTimeoutMs, new RetryPolicy() {
+
+    private String connectString = "192.168.188.129";
+    private int sessionTimeoutMs = 5000;
+    private int connectionTimeoutMs = 5000;
+
+    private CuratorFramework client;
+
+    @Before
+    public void before() {
+        client = CuratorFrameworkFactory.newClient(connectString, sessionTimeoutMs, connectionTimeoutMs, new RetryPolicy() {
             @Override
             public boolean allowRetry(int retryCount, long elapsedTimeMs, RetrySleeper sleeper) {
                 return true;
@@ -21,10 +25,34 @@ public class ConnectTest {
         });
         client.start();
         System.out.println("zk client start successfully!");
-        
-        
-        System.out.println(client.getChildren().forPath("/"));
-        
     }
+
+    @Test
+    public void createNode() throws Exception {
+        client.create().creatingParentsIfNeeded().forPath("/0001/0002", "curator".getBytes());
+
+        System.out.println(client.getChildren().forPath("/0001"));
+
+        System.out.println(client.getData().forPath("/0001/0002"));
+    }
+    
+    @Test
+    public void removeNode() throws Exception{
+        client.delete().forPath("/0001/0002");
+        
+        System.out.println(client.getChildren().forPath("/0001"));
+    }
+    
+    @Test
+    public void modifyData() throws Exception{
+        
+        client.setData().forPath("/0001/0002", "888".getBytes());
+        
+        System.out.println(client.getData().forPath("/0001/0002"));
+    }
+    
+    
+    
+    
 
 }
